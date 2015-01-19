88c59b4717cdf0cb5c35dc02a8e90062f301dcf4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-01-20 07:49:26 +1100

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress()
+]);
+
+// Generate mermaid diagrams for '.mermaid' elements
+// https://github.com/knsv/mermaid
+require('d3');
+require('dagre-d3');
+require('mermaid');
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
+
